8ac5f67a498ed249866ff8fe546f3a0c16581c3f
cheesy d <cheesyd64@hotmail.com>
2023-04-14 15:42:08 -0700

    Added Identity and username to model and view

--- RecipeWebsite/Program.cs
+++ RecipeWebsite/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RecipeWebsite.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("RecipeWebsiteContextConnection") ?? throw new InvalidOperationException("Connection string 'RecipeWebsiteContextConnection' not found.");
+
+builder.Services.AddDbContext<RecipeWebsiteContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<RecipeWebsiteContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -16,11 +26,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
