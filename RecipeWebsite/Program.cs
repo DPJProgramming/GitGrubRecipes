1cc1a4fd59bab878e6e745f33325ea23cb9f7405
cheesy d <cheesyd64@hotmail.com>
2023-04-15 13:47:06 -0700

    Added identity register/login options and changed some requirements in program.cs

--- RecipeWebsite/Program.cs
+++ RecipeWebsite/Program.cs
@@ -13,6 +13,28 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//can configure password requirements here
+builder.Services.Configure<IdentityOptions>(options => {
+
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 0;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
