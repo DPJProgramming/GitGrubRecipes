88da8dba8d79f8e2d969dc4a9ffafe8ffb4a4fa8
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-04-17 13:26:40 -0700

    Added documentation for Recipe and Comment classes

--- RecipeWebsite/Models/Comment.cs
+++ RecipeWebsite/Models/Comment.cs
@@ -2,19 +2,37 @@
 
 namespace RecipeWebsite.Models
 {
+    /// <summary>
+    /// Represents a user's submitted commment
+    /// </summary>
     public class Comment
     {
+        /// <summary>
+        /// Unique identifier for comment
+        /// </summary>
         [Key]
         public int CommentId { get; set; }
 
+        /// <summary>
+        /// Unique identifier for recipe
+        /// NOTE: Since comments are stored in recipe objects, the recipe ID might be redundant.
+        /// </summary>
         public int RecipeId { get; set; }
         
+        /// <summary>
+        /// Unique identifier for user that submitted the comment
+        /// </summary>
         public int UserId { get; set; }
 
-        // max length?
+        /// <summary>
+        /// The text content of the submitted comment
+        /// </summary>
         [Required]
         public string Content { get; set; }
 
+        /// <summary>
+        /// An integer that stores the number of votes/likes a comment has
+        /// </summary>
         public int Votes { get; set; }
     }
 }
