b0609983b3be947c8fb6c729bf0e82d095d353c9
cheesy d <cheesyd64@hotmail.com>
2023-04-22 16:12:20 -0700

    Adjusted links in layout view and program.cs to load RecipeControllers Index

---
+++ RecipeWebsite/AddInitialRecipesQuery.sql
@@ -0,0 +1,5 @@
+INSERT INTO Recipe(Title, UserId, ImageUrl, Ingredients, Directions)
+VALUES('Muffin', 1, 'www.google.com', '1 cup flour, 1cup sugar', 'Mix the flour and sugar together')
+
+INSERT INTO Recipe(Title, UserId, ImageUrl, Ingredients, Directions)
+VALUES('Pie', 2, 'www.yahoo.com', '1 cup flour, 1cup sugar, 1/2 cup fruit', 'Mix the flour and sugar together. Then add the fruit in a pie dish')
