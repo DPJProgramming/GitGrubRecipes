bde62d34b25c4130af83948b6c69de9f8cfc59fe
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-05-27 10:44:15 -0700

    removed some comments

--- RecipeWebsite/Areas/Identity/Data/RecipeWebsiteContext.cs
+++ RecipeWebsite/Areas/Identity/Data/RecipeWebsiteContext.cs
@@ -19,7 +19,7 @@ public class RecipeWebsiteContext : IdentityDbContext<User>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
-        
+
         builder.Entity<Recipe>()
             .HasOne<User>(r => r.Author)
             .WithMany(u => u.MyRecipes);
@@ -40,27 +40,6 @@ public class RecipeWebsiteContext : IdentityDbContext<User>
                 x.HasKey("UserId", "RecipeId");
                 x.ToTable("FavoriteRecipes");
             });
-
-        //previous code before adding FavoriteRecipe
-       /* builder.Entity<Recipe>()
-        .HasMany(r => r.UsersFavorited)
-        .WithMany(u => u.MyFavorites)
-        .UsingEntity<Dictionary<string, object>>(
-            "FavoriteRecipe",
-            x => x.HasOne<User>().WithMany().HasForeignKey("UserId").OnDelete(DeleteBehavior.Cascade),
-            x => x.HasOne<Recipe>().WithMany().HasForeignKey("RecipeId").OnDelete(DeleteBehavior.NoAction),
-            x => {
-                x.HasKey("UserId", "RecipeId");
-                x.ToTable("FavoriteRecipes");
-            }) ;*/
-
-
-        //attempt at configuring FavoriteRecipe could delete in future
-        /*builder.SharedTypeEntity<Dictionary<string, object>>(
-            "FavoriteRecipes", fr => {
-                fr.Property<string>("UserId");
-                fr.Property<int>("RecipeId");
-            });*/
     }
 
     public DbSet<RecipeWebsite.Models.Recipe>? Recipe { get; set; }
