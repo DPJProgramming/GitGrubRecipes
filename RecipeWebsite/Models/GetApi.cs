2ac973a5227d16b27c035c227efbfaef1a84c38d
cheesy d <cheesyd64@hotmail.com>
2023-08-20 22:53:11 -0700

    Added GetApi class

---
+++ RecipeWebsite/Models/GetApi.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeWebsite.Models {
+    public static class GetApi {
+        public static string apiKey() {
+                    var builder = WebApplication.CreateBuilder();
+                    return builder.Configuration.GetSection("NutritionApi").Value;
+                }
+        }
+    }
