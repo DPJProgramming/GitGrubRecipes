56e064e5856a831445cfbb6f541addcc3156a0c6
cheesy d <cheesyd64@hotmail.com>
2023-08-26 11:47:42 -0700

    Fixed same email for multiple accounts

--- RecipeWebsite/Program.cs
+++ RecipeWebsite/Program.cs
@@ -34,7 +34,7 @@ builder.Services.Configure<IdentityOptions>(options => {
     // User settings.
     options.User.AllowedUserNameCharacters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
+    options.User.RequireUniqueEmail = true;
 });
 
 var app = builder.Build();
